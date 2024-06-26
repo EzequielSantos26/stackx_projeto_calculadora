5dfdd1a315117ebceafd2bf15f8c4acd0142e138
Seu nome <ezequieldossantosribeiro@gmailcom>
2024-06-25 22:31:04 -0300

    css parte 2 ejs parte 1

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+const powerButton = document.querySelector('[data-power]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton= document.querySelector('[data-delete]');
+const numbersButtons = document.querySelectorAll('[data-number]');
+const allclearButton = document.querySelector('[data-all-clear]');
+const history = document.querySelector('[data-history-container]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const dataContainer = document.querySelector('[data-result-date-container]');
+const currentOperandText = document.querySelector('[data-current-operation]');
+const previousOperandText = document.querySelector('[data-previous-operation]');
+
+
+
+console.log(previousOperandText);
