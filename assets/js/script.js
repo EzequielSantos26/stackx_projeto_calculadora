40bf199ecb00d33250cb81a983c33dabedd84e1f
Seu nome <ezequieldossantosribeiro@gmailcom>
2024-06-25 23:12:25 -0300

    js part2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+import Calculator from './Calculator.js';
 'use strict'
 const powerButton = document.querySelector('[data-power]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -9,7 +10,8 @@ const operationButtons = document.querySelectorAll('[data-operator]');
 const dataContainer = document.querySelector('[data-result-date-container]');
 const currentOperandText = document.querySelector('[data-current-operation]');
 const previousOperandText = document.querySelector('[data-previous-operation]');
+const calculator = new Calculator();
+
+console.log (calculator); 
 
 
-
-console.log(previousOperandText);
