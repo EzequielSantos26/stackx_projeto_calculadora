881d091da14e690548a71ccbba115ae4bfd7f50e
Seu nome <ezequieldossantosribeiro@gmailcom>
2024-06-27 23:02:25 -0300

    js parte 3

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -1,1 +1,57 @@
-export default class Calculator{} 
+export default class Calculator { 
+    constructor(previousOperandText,currentOperandText,dataContainer,history) {
+        this.previousOperandText=previousOperandText
+        this.currentOperandText=currentOperandText
+        this.dataContainer=dataContainer
+        this.history=history 
+        this.clear()
+    } 
+    clear (){
+        this.currentOperand = ""
+        this.previousOperand = ""
+        this.Operation = undefined 
+        
+    }
+    appendNumber(number) { 
+       if(this.currentOperand.includes(".") && number === ".") {
+        return
+       }
+       this.currentOperand = `${this.currentOperand}${number.toString()}`
+    }
+     
+    updateDisplay(){
+       this.previousOperandText.innerText = `${this.formatDisplayNumber(this.previousOperand)} ${this.Operation || ""}`
+
+       this.currentOperandText.innerText = this.formatDisplayNumber(this.currentOperand)
+    }
+    formatDisplayNumber(number){
+       
+        const stringNumber = number.toString()
+
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+
+        const decimalDigts = stringNumber.split("1")[1]
+
+        let intergerDisplay
+
+        if (isNaN(integerDigits)) {
+            intergerDisplay = "" 
+        }
+            else{ 
+                intergerDisplay = integerDigits.toLocaleString("en", {maximumFractionDigits:0})
+            }
+
+          if(decimalDigts != null){
+            return `${intergerDisplay}.${decimalDigts}`
+          }  
+
+          else{ 
+            return intergerDisplay
+          }
+
+    }
+
+    deleteDigit(){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,33 @@
-import Calculator from './Calculator.js';
-'use strict'
+import  Calculator from "./Calculator.js"
 const powerButton = document.querySelector('[data-power]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton= document.querySelector('[data-delete]');
-const numbersButtons = document.querySelectorAll('[data-number]');
+const numberButtons = document.querySelectorAll('[data-number]');
 const allclearButton = document.querySelector('[data-all-clear]');
 const history = document.querySelector('[data-history-container]');
 const operationButtons = document.querySelectorAll('[data-operator]');
 const dataContainer = document.querySelector('[data-result-date-container]');
 const currentOperandText = document.querySelector('[data-current-operation]');
 const previousOperandText = document.querySelector('[data-previous-operation]');
-const calculator = new Calculator();
 
-console.log (calculator); 
+const calculator = new Calculator ( previousOperandText,currentOperandText,dataContainer,history) 
+
+numberButtons.forEach(
+    (  numberButton)=>{ 
+        numberButton.addEventListener("click",()=>{ 
+            calculator.appendNumber(numberButton.innerText)
+            calculator.updateDisplay() 
+        })
+
+    }
+); 
+
+deleteButton.addEventListener("click",()=> {
+    calculator.deleteDigit()
+    calculator.updateDisplay()
+}) 
 
 
+
+
+//()=> {}
