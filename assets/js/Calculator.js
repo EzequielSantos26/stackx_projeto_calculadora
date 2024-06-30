f7144d726ec28bc9e4eb3b99fa9919b9f1464e17
Seu nome <ezequieldossantosribeiro@gmailcom>
2024-06-30 00:50:20 -0300

    js parte final

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -1,57 +1,155 @@
-export default class Calculator { 
-    constructor(previousOperandText,currentOperandText,dataContainer,history) {
-        this.previousOperandText=previousOperandText
-        this.currentOperandText=currentOperandText
-        this.dataContainer=dataContainer
-        this.history=history 
-        this.clear()
-    } 
-    clear (){
-        this.currentOperand = ""
-        this.previousOperand = ""
-        this.Operation = undefined 
-        
+export default class Calculator {
+  constructor(previousOperandText, currentOperandText, dataContainer, history) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+    this.dataContainer = dataContainer;
+    this.history = history;
+    this.clear();
+  }
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  appendNumber(number) {
+    if (this.currentOperand.includes(".") && number === ".") {
+      return;
     }
-    appendNumber(number) { 
-       if(this.currentOperand.includes(".") && number === ".") {
-        return
-       }
-       this.currentOperand = `${this.currentOperand}${number.toString()}`
+    this.currentOperand = `${this.currentOperand}${number.toString()}`;
+  }
+
+  updateDisplay() {
+    this.previousOperandText.innerText = `${this.formatDisplayNumber(
+      this.previousOperand
+    )} ${this.operation || ""}`;
+
+    this.currentOperandText.innerText = this.formatDisplayNumber(
+      this.currentOperand
+    );
+  }
+  formatDisplayNumber(number) {
+    const stringNumber = number.toString();
+
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+
+    const decimalDigts = stringNumber.split("1")[1];
+
+    let intergerDisplay;
+
+    if (isNaN(integerDigits)) {
+      intergerDisplay = "";
+    } else {
+      intergerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
     }
-     
-    updateDisplay(){
-       this.previousOperandText.innerText = `${this.formatDisplayNumber(this.previousOperand)} ${this.Operation || ""}`
 
-       this.currentOperandText.innerText = this.formatDisplayNumber(this.currentOperand)
+    if (decimalDigts != null) {
+      return `${intergerDisplay}.${decimalDigts}`;
+    } else {
+      return intergerDisplay;
     }
-    formatDisplayNumber(number){
-       
-        const stringNumber = number.toString()
+  }
 
-        const integerDigits = parseFloat(stringNumber.split(".")[0])
+  deleteDigit() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 
-        const decimalDigts = stringNumber.split("1")[1]
+  chooseOperation(operation) {
+    if (this.previousOperand !== "") {
+      this.calculate();
+    }
 
-        let intergerDisplay
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = " ";
+  }
+  calculate() {
+    let result;
+    const resultHistory = {
+      data: [],
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocaleTimeString(),
+    };
+    const previousOperandFloat = parseFloat(this.previousOperand);
+    const currentOperandFloat = parseFloat(this.currentOperand);
+    const operationSynbos = {
+      adition: "+",
+      subtraction: "-",
+      multiplication: "*",
+      division: "/",
+    };
+    if (isNaN(previousOperandFloat) || isNaN(currentOperandFloat)) {
+      return;
+    }
 
-        if (isNaN(integerDigits)) {
-            intergerDisplay = "" 
+    switch (this.operation) {
+      case operationSynbos.adition:
+        result = previousOperandFloat + currentOperandFloat;
+        break;
+      case operationSynbos.subtraction:
+        result = previousOperandFloat - currentOperandFloat;
+        break;
+      case operationSynbos.multiplication:
+        result = previousOperandFloat * currentOperandFloat;
+        break;
+      case operationSynbos.division:
+        if (currentOperandFloat === 0) {
+          alert("não é possivel realizar divizão por 0");
+          return;
         }
-            else{ 
-                intergerDisplay = integerDigits.toLocaleString("en", {maximumFractionDigits:0})
-            }
-
-          if(decimalDigts != null){
-            return `${intergerDisplay}.${decimalDigts}`
-          }  
-
-          else{ 
-            return intergerDisplay
-          }
-
+        result = previousOperandFloat / currentOperandFloat;
+        break;
+      default:
+        break;
     }
 
-    deleteDigit(){
-        this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    this.currentOperand = result;
+    resultHistory.data.push(
+      previousOperandFloat,
+      this.operation,
+      currentOperandFloat,
+      result
+    );
+    if (this.dataContainer.childNodes.length > 3) {
+      this.dataContainer.innerHTML = "";
+    }
+    this.createDataContainer(resultHistory);
+    this.Operation = undefined;
+    this.previousOperand = "";
+    this.currentOperand = "";
+  }
+  createDataContainer(resultHistory) {
+    this.history.classList.remove("class-hide");
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("class-result-date");
+    const divData = document.createElement("div");
+    const labelDate = document.createElement("label");
+    labelDate.innerHTML += '<i class="bi bi-calendar3"></i>';
+    labelDate.innerHTML += `<input disable style="background-color: #ffff " type="text" value=${resultHistory.date}> `;
+
+    const labelTime = document.createElement("label");
+    labelTime.innerHTML += '<i class="bi bi-alarm"></i>';
+    labelTime.innerHTML += `<input disable style="background-color: #ffff " type="text" value=${resultHistory.time}> `;
+
+    const labelResult = document.createElement("label");
+    labelResult.innerHTML +=
+      '<i style="cursor: help" class="bi bi-calculator-fill"></i>';
+    labelResult.innerHTML += `<input class="class-result" readonly style="background-color: #ffff; cursor:help" type="text" value="${resultHistory.data[0]} ${resultHistory.data[1]} ${resultHistory.data[2]}=${resultHistory.data[3]}" title="click para reutilizar o resultado: ${resultHistory.data[3]}">`; 
+
+    const labelTrash = document.createElement("label");
+    labelTrash.innerHTML += '<i style="cursor: pointer" class="bi bi-trash3"></i>';
+    labelTrash.innerHTML += `<input readonly class="class-delete" style="cursor:pointer " type="text" value="delete"> `;
+
+    divData.appendChild(labelDate) 
+    divData.appendChild(labelTime) 
+    divData.appendChild(labelResult)
+    divData.appendChild(labelTrash)
+    divContainer.appendChild(divData)
+    this.dataContainer.appendChild(divContainer)
+  }
+    insertDisplay(number){
+        this.currentOperand=number
+        this.updateDisplay()
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,78 @@ const operationButtons = document.querySelectorAll('[data-operator]');
 const dataContainer = document.querySelector('[data-result-date-container]');
 const currentOperandText = document.querySelector('[data-current-operation]');
 const previousOperandText = document.querySelector('[data-previous-operation]');
-
+let isCalculatorON=false
 const calculator = new Calculator ( previousOperandText,currentOperandText,dataContainer,history) 
-
 numberButtons.forEach(
     (  numberButton)=>{ 
         numberButton.addEventListener("click",()=>{ 
+            if(isCalculatorON===false){
+                alert("A calculadora esta desligada")
+                return
+             }
             calculator.appendNumber(numberButton.innerText)
             calculator.updateDisplay() 
         })
 
     }
 ); 
-
-deleteButton.addEventListener("click",()=> {
+deleteButton.addEventListener("click",()=> { 
+    if(isCalculatorON===false){
+        alert("A calculadora esta desligada")
+        return }
     calculator.deleteDigit()
     calculator.updateDisplay()
 }) 
 
+operationButtons.forEach((operation)=>{
+    operation.addEventListener("click",()=> {
+        if(isCalculatorON===false){
+            alert("A calculadora esta desligada")
+            return }
+       calculator.chooseOperation(operation.innerText)
+       calculator.updateDisplay()
+    })
+})
+
+powerButton.addEventListener("click",()=> {
+    if ( isCalculatorON ){
+        if(!confirm("realmente deseja desligar a calculadora"))return
+    }
+    isCalculatorON =!isCalculatorON
+    powerButton.classList.toggle("class-active") 
+    history.classList.toggle("class-hide")
+    !isCalculatorON?location.reload():""
+})
+
+allclearButton.addEventListener("click",()=> {
+    if(isCalculatorON===false){
+        alert("A calculadora esta desligada")
+        return
+     }
+     calculator.clear()
+     calculator.updateDisplay()
+})
+    equalsButton.addEventListener("click",()=>{
+        if(isCalculatorON===false){
+            alert("A calculadora esta desligada")
+            return
+         }
+         calculator.calculate()
+         calculator.updateDisplay()
+         calculator.clear()
+    })
+    document.addEventListener("click",(event)=> {
+    const target= event.target
+    const parentElement=target.closest('div')
+    if(target.classList.value.includes("class-result")){
+        calculator.insertDisplay(target.value.split("=")[1].trim())
+    }
+    if(target.classList.contains("class-delete")) {
+        if(!confirm("Realmente deseja excluir o resgistro?")) return
+        parentElement.remove() 
+    }
+    })
 
 
-
+// === || !== // &&
 //()=> {}
